e197a30f85a84c69a7e71e92e03ab7fe98874783
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2022-10-31 21:31:12 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,11 @@
+//Take a number(n) from user & output its cube(n*n*n).
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    printf("Cube : %d",n*n*n);
+    return 0;
+}
