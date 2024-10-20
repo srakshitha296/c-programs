eb2bf8406480b5e42e71ba5eb3f6a8bc7f56555d
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2024-10-20 17:51:35 +0000

    Program to reverse an array

--- reverseArray.c
+++ reverseArray.c
@@ -10,13 +10,12 @@ int main(){
         for(int i=0; i<n; i++)
             arr[i] = temp[n-1-i];
 
-        for(int i=0; i<n; i++)
-            printf("%d",arr[i]);
-
     }
 
     int arr[] = {1,2,3,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     reverse(arr, n);
+     for(int i=0; i<n; i++)
+            printf("%d",arr[i]);
     return 0;
 }
